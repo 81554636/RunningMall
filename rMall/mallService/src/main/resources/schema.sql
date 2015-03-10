e2e6d549bd3848f2a4c87f1d152289c01208e29e
ryo.hune <ryo.hune@gmail.com>
2015-03-10 09:33:54 +0800

    Portal: 	修复, 页面控件不能点

--- rMall/mallService/src/main/resources/schema.sql
+++ rMall/mallService/src/main/resources/schema.sql
@@ -1,6 +1,16 @@
-insert into rmall_station(id,displayName,city,createdate,username,password) values(1,'南京小站','南京',CURRENT_TIMESTAMP,'SUA','SUA');
-insert into rmall_station(id,displayName,city,createdate,username,password) values(2,'上海小站','上海',CURRENT_TIMESTAMP,'HUA','HUA');
-insert into rmall_station(id,displayName,city,createdate,username,password) values(3,'杭州小站','杭州',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+CREATE DATABASE `runningMall` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+
+insert into rmall_station(displayName,city,createdate,username,password) values('余姚','余姚',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('慈溪','慈溪',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('北仑','北仑',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('镇海','镇海',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('鄞州','鄞州',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('江东','江东',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('江北','江北',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('海曙','海曙',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('奉化','奉化',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('宁海','宁海',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
+insert into rmall_station(displayName,city,createdate,username,password) values('象山','象山',CURRENT_TIMESTAMP,'ZHEA','ZHEA');
 	
 insert into rmall_product(id,displayName,price,createDate,lastUpdate) values(1,'wine红酒',230.00,current_timestamp,current_timestamp);
 insert into rmall_product(id,displayName,price,createDate,lastUpdate) values(2,'kiwi奇异果',120.00,current_timestamp,current_timestamp);
